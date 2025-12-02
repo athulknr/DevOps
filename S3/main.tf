5b8a558367d9c0874f5257f69b2942393ee9bc65
Athul K <athulkannoth9@gmail.com>
2025-12-02 23:09:37 +0530

    S3 modification

--- S3/main.tf
+++ S3/main.tf
@@ -20,15 +20,16 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "us_bucket_athul" {
-  bucket = "devops-athul-bucket"
+  bucket = "devops-athul-canada"
+}
+
+resource "aws_s3_bucket" "ohio_bucket" {
+  bucket = "devops-ohio-devops"
+  provider = aws.south-2
 }
 
 resource "aws_s3_bucket" "mumbai_bucket_athul" {
-  bucket = "devops-athul-"
+  bucket = "devops-athul-west1"
   provider = aws.west-1
 }
 
-resource "aws_s3_bucket" "ohio_bucket_athul" {
-  bucket = "devops-bucket-ohio"
-  provider = aws.south-2
-}
