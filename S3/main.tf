d1bf774f58140a72352f312c3fe84f03449d3607
Athul K <athulkannoth9@gmail.com>
2025-12-03 22:40:50 +0530

    S3_alias

--- S3/main.tf
+++ S3/main.tf
@@ -42,19 +42,3 @@ resource "aws_s3_bucket" "mumbai_bucket_sydney" {
   bucket   = "devops-athul-sydney"
   provider = aws.sydney
 }
-
-resource "aws_vpc" "athul_vpc" {
-  cidr_block = "10.0.0.0/24"
-  tags = {
-    Name = "demo-vpc-athul"
-  }
-}
-
-resource "aws_subnet" "net1" {
-  vpc_id            = aws_vpc.athul_vpc.id
-  cidr_block        = "10.0.0.0/27"
-  availability_zone = "ca-central-1a"
-  tags = {
-    Name = "Demo-sub"
-  }
-}
